d36fb936823aaa1f06ab3bb30e269c644eff5599
yveszangpat <Evenerak2547@gmail.com>
2025-10-07 03:23:38 +0700

    ฝากใส่รูปด้วย

--- src/models/TaskImage.js
+++ src/models/TaskImage.js
@@ -4,7 +4,8 @@ const taskImageSchema = new mongoose.Schema({
     task_id: { type: mongoose.Schema.Types.ObjectId, required: true },
     image_path: { type: String, required: true },
     added_by: { type: String, enum: ["user", "technician"], required: true },
-    created_at: { type: Date, default: Date.now }
+    created_at: { type: Date, default: Date.now },
+    description: { type: String }
 })
 
 const TaskImage = mongoose.model("task_images", taskImageSchema)
